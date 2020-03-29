c3745088dbef803e50e901c07e24de8426f31a35
Ryan Gostic <rgostic22@gmail.com>
2020-03-28 23:41:29 -0400

    added missing crypto logos, small style changes

--- part/front-page/top-fold.php
+++ part/front-page/top-fold.php
@@ -3,15 +3,15 @@
 
 <div class="top-fold">
     <h1>WooCommerce Bitcoin and Cryptocurrency Payment Gateway</h1>
-    <div class="container fluid">
+    <div class="container">
         <div class="row">
-            <div class="columns large-offset-5">
-        <p>No Fees</p>
-        <p>No Registration</p>
-        <p>No Logins</p>
-        <p>No Middleman</p>
-                </div>
+            <div class="columns large-offset-4">
+                <p>No Fees</p>
+                <p>No Registration</p>
+                <p>No Logins</p>
+                <p>No Middleman</p>
             </div>
+        </div>
     </div>
 </div>
 
